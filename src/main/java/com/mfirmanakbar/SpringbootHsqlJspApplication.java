281fca86afe74cc0e2c9d6ae32a2a98648b6b89c
mfirmanakbar <mu.firman.akbar@gmail.com>
2019-05-31 20:09:19 +0700

    first commit

---
+++ src/main/java/com/mfirmanakbar/SpringbootHsqlJspApplication.java
@@ -0,0 +1,13 @@
+package com.mfirmanakbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHsqlJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHsqlJspApplication.class, args);
+	}
+
+}
